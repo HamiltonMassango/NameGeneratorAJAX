ab7d6b41791cfee506456f221e5c12c6054d6a8c
HamiltonMassango <hamiltonsilva551@gmail.com>
2020-04-06 20:43:26 +0100

    Falta um pouco

--- js/app.js
+++ js/app.js
@@ -1,32 +1,58 @@
 document.querySelector('#generate-names').addEventListener('submit', loadNames);
 
+
+
 // Execute the function to query the API
-function loadNames(event) {
-    event.preventDefault();
+function loadNames(e) {
+     e.preventDefault();
 
-    // Read the values from the form creae the variaveis 
-    const origin = document.querySelector('#country').value;
-    const genre = document.querySelector('#genre').value;
-    const amount = document.querySelector('#quantity').value;
 
-    // Build the URL
-    let url = 'http://uinames.com/api?';
-    
-    // Read the origin and append to the url
+     // Read the values from the form and create the variables
+     const origin = document.getElementById('country').value;
+     const genre = document.getElementById('genre').value;
+     const amount = document.getElementById('quantity').value;
 
-    if(origin){
-        url += `region=${origin}&`;
-    }
+     // Build the URL
+     let url = 'http://uinames.com/api/?';
+     // Read the origin and append to the url
+     if(origin !== ''){
+          url += `region=${origin}&`;
+     }
+     // Read the genre and append to the url
+     if(genre !== ''){
+          url += `gender=${genre}&`;
+     }    
+      // Read the amount and append to the url
+     if(amount !== ''){
+          url += `amount=${amount}&`;
+     }
+     
+     // Ajax Call
+     const xhr = new XMLHttpRequest();
 
-    // Read the genre and append to the url
+     // Open the connection
+     xhr.open('GET', url, true );
 
-    if(genre){
-        url += `gender=${genre}&`;
-    }
+     // Execute the function
+     xhr.onload = function() {
+          if(this.status === 200) {
+               const names = JSON.parse( this.responseText );
+               
+               // Insert into the HTML
 
-    // Read the amount and append to the url
+               let html = '<h2>Generated Names</h2>';
+               html += '<ul class="list">';
+               names.forEach(function(name){
+                    html += `
+                         <li>${name.name}</li>
+                    `;
+               });
+               html += '</ul>';
 
-    if(amount){
-        url += `amount=${amount}&`;
-    }
+               document.querySelector('#result').innerHTML = html;
+          }
+     }
+
+     // Send the request
+     xhr.send();
 }
